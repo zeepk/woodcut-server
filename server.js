1c0229bf3cf3d10bcb98ce6862e1259591ec5295
Matt Hughes <zeepkdb@gmail.com>
2020-09-07 21:09:51 -0700

    cors

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
 
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+
+	next();
+});
 app.use(express.json());
 
 const usersRouter = require('./routes/users');
