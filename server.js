66d875edee9b6a11b0664c99cee3768bb5dea059
Matt Hughes <zeepkdb@gmail.com>
2020-11-24 21:33:53 -0800

    port message

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use('/activities', activitiesRouter);
 const clansRouter = require('./routes/clans');
 app.use('/clans', clansRouter);
 
-app.listen(process.env.PORT || 8000, () =>
-	console.log('Server started on port 8000')
-);
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
